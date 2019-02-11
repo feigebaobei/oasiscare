5ee44a07fbc1183646643fd7cd6567eecaf471c5
feigebaobei <18515195415@163.com>
2019-02-11 18:23:00 +0800

    00

--- src/components/titleArea/index.vue
+++ src/components/titleArea/index.vue
@@ -1,12 +1,80 @@
 <template>
   <div class="titleArea">
-    titleArea
+    <!-- titleArea -->
+    <div class="titleBox">
+      <h2 v-html="title"></h2>
+    </div>
+    <div class="subTitleBox" v-if="subTitle">
+      <span v-html="subTitle"></span>
+    </div>
+    <div class="moreBox">
+      <span v-html="moreText"></span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: {},
+  props: {
+    hasImg: {
+      type: Boolean;
+      default: true
+    },
+    imgUrl: {
+      type: String;
+      default: ''
+    },
+    title: {
+      type: String;
+      default: ''
+    },
+    subTitle: {
+      type: String;
+      default: ''
+    },
+    moreText: {
+      type: String;
+      default: ''
+    },
+    hasMore: {
+      type: Boolean;
+      default: true
+    },
+    hasDirectionIcon: {
+      type: Boolean;
+      default: true
+    },
+    directionImgUrl: {
+      type: String;
+      default: ''
+    },
+    directionImgSwitchDirection: {
+      type: Boolean;
+      default: true
+    },
+    directionImgDirection: {
+      type: String;
+      default: '90deg'
+    },
+    eventType: {
+      type: String;
+      default: ''
+    },
+    data: {
+      default () {
+        return {}
+      }
+    },
+    eventTypeMore: {
+      type: String;
+      default: ''
+    },
+    dataMore: {
+      default () {
+        return {}
+      }
+    }
+  },
   data () {
     return {
     }
@@ -22,8 +90,10 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import '@/assets/style/main.styl'
   
   .titleArea
-    color: green
+    display: flex
+    padding: $paddingTop $paddingRight 0 $paddingLeft
 
 </style>
