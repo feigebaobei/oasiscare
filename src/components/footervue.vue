44caa02b80f8bec55854481c1f9429f66567ea95
feigebaobei <18515195415@163.com>
2019-02-12 11:49:53 +0800

    添加footer组件

---
+++ src/components/footervue.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="footervue">
+    footervue footervue
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  components: {
+  },
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+
+<style lang="stylus" scoped>
+
+.footervue
+  height: 90px
+  font-size: 28px
+  line-height: 90px
+  padding: 0 30px
+  background-color: #828488
+
+</style>
