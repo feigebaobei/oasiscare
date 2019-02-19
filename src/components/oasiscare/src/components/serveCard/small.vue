f8d3d013f764f24aef38e6dbfe154131a041d4fb
feigebaobei <18515195415@163.com>
2019-02-19 18:08:43 +0800

    这个组件被ui删除了，无法进行开发。

---
+++ src/components/oasiscare/src/components/serveCard/small.vue
@@ -0,0 +1,30 @@
+<!-- 这个组件被ui删除了 -->
+<template>
+  <div class="small">
+    small
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  components: {
+  },
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+
+<style lang="stylus" scoped>
+
+.small
+
+
+</style>
