7f16d1e8c9b1224c4333636f5191140ebd5ba541
feigebaobei <18515195415@163.com>
2019-02-19 16:05:15 +0800

    编写label的demo

--- src/components/demo/common/index.vue
+++ src/components/demo/common/index.vue
@@ -1,32 +1,48 @@
 <template>
   <div class="common">
     <h2>imgText</h2>
-    <img-text :imgUrl="a0.imgUrl" :text="a0.text"></img-text>
-    <img-text :imgUrl="a1.imgUrl" :text="a1.text"></img-text>
+    <img-text :imgUrl="dataImgText0.imgUrl" :text="dataImgText0.text"></img-text>
+    <img-text :imgUrl="dataImgText1.imgUrl" :text="dataImgText1.text"></img-text>
+    <h2>label</h2>
+    <div class="labelBox">
+      <label-vue :keyItem="dataLabel0.keyItem"></label-vue>
+      <label-vue :keyItem="dataLabel0.keyItem" :keyAlign="dataLabel0.keyAlign"></label-vue>
+      <label-vue :keyItem="dataLabel0.keyItem" :keyAlign="'right'"></label-vue>
+      <label-vue :keyItem="dataLabel0.keyItem" :keyAlign="'space-around'"></label-vue>
+      <label-vue :keyItem="dataLabel0.keyItem" :keyAlign="'space-between'"></label-vue>
+      <label-vue :keyItem="dataLabel0.keyItem" :isBold="dataLabel0.isBold"></label-vue>
+    </div>
   </div>
 </template>
 
 <script>
-import { imgText } from '@/components/oasiscare'
+import { imgText, label } from '@/components/oasiscare'
 import { icon } from '@/lib/picMap.js'
 export default {
   props: {},
   data () {
     return {
-      a0: {
+      dataImgText0: {
         imgUrl: icon.logo.x1,
         text: 'text'
       },
-      a1: {
+      dataImgText1: {
         imgUrl: icon.test.x1,
         text: '文本'
+      },
+      dataLabel0: {
+        keyW: '2rem',
+        keyAlign: 'center',
+        keyItem: '飞机坦克',
+        isBold: true,
       }
     }
   },
   computed: {
   },
   components: {
-    imgText
+    imgText,
+    labelVue: label
   },
   methods: {},
   created () {},
@@ -40,5 +56,8 @@ export default {
 
     h2
       font-size: .36rem
+
+    .labelBox
+      font-size: .28rem
 
 </style>
