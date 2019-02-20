e9a26d45f2594de4cbe196db099bf4103358619b
feigebaobei <18515195415@163.com>
2019-02-20 11:20:55 +0800

    修改hardCorner的demo实例

--- src/components/demo/hardCorner.vue
+++ src/components/demo/hardCorner.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="hardCorner">
     <h2>基本样式</h2>
-    <hard-corner4 :text="a0.text" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg" :area="a0.area"></hard-corner4>
-    <hard-corner4 :text="'小标准'" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg" :area="a0.area"></hard-corner4>
-    <hard-corner4 :text="a0.text" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg" :area="a1.area"></hard-corner4>
-    <hard-corner4 :text="'中标准'" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg" :area="a1.area"></hard-corner4>
-    <hard-corner4 :text="a0.text" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg"></hard-corner4>
-    <hard-corner4 :text="'大标准'" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg"></hard-corner4>
+    <hard-corner :text="a0.text" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg" :area="a0.area"></hard-corner>
+    <hard-corner :text="'小标准'" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg" :area="a0.area"></hard-corner>
+    <hard-corner :text="a0.text" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg" :area="a1.area"></hard-corner>
+    <hard-corner :text="'中标准'" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg" :area="a1.area"></hard-corner>
+    <hard-corner :text="a0.text" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg"></hard-corner>
+    <hard-corner :text="'大标准'" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg"></hard-corner>
     <h2>改变bg</h2>
-    <hard-corner4 :text="a0.text" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg" :area="a0.area"></hard-corner4>
-    <hard-corner4 :text="a1.text" :color="a1.color" :border="a1.border" :borderRadius="a1.borderRadius" :bg="a1.bg" :area="a1.area"></hard-corner4>
-    <hard-corner4 :text="a2.text" :color="a2.color" :border="a2.border" :borderRadius="a2.borderRadius" :bg="a2.bg" :area="a2.area"></hard-corner4>
-    <hard-corner4 :text="a2.text" :color="a2.color" :border="a2.border" :borderRadius="a2.borderRadius" :area="a2.area"></hard-corner4>
+    <hard-corner :text="a0.text" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg" :area="a0.area"></hard-corner>
+    <hard-corner :text="a1.text" :color="a1.color" :border="a1.border" :borderRadius="a1.borderRadius" :bg="a1.bg" :area="a1.area"></hard-corner>
+    <hard-corner :text="a2.text" :color="a2.color" :border="a2.border" :borderRadius="a2.borderRadius" :bg="a2.bg" :area="a2.area"></hard-corner>
+    <hard-corner :text="a2.text" :color="a2.color" :border="a2.border" :borderRadius="a2.borderRadius" :area="a2.area"></hard-corner>
     <h2>改变border</h2>
-    <hard-corner4 :text="a0.text" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg" :area="a0.area" :maxW="a0.maxW" :minW="a0.minW"></hard-corner4>
-    <hard-corner4 :text="a0.text" :color="a0.color" :bg="a0.bg" :area="a0.area"></hard-corner4>
+    <hard-corner :text="a0.text" :color="a0.color" :border="a0.border" :borderRadius="a0.borderRadius" :bg="a0.bg" :area="a0.area" :maxW="a0.maxW" :minW="a0.minW"></hard-corner>
+    <hard-corner :text="a0.text" :color="a0.color" :bg="a0.bg" :area="a0.area"></hard-corner>
     <h2>改变border-radius</h2>
-    <hard-corner4 :text="a0.text" :color="a0.color" :bg="a0.bg" :borderRadius="a0.borderRadius" :area="a0.area"></hard-corner4>
-    <hard-corner4 :text="a1.text" :color="a1.color" :bg="a1.bg" :borderRadius="a1.borderRadius" :area="a1.area"></hard-corner4>
+    <hard-corner :text="a0.text" :color="a0.color" :bg="a0.bg" :borderRadius="a0.borderRadius" :area="a0.area"></hard-corner>
+    <hard-corner :text="a1.text" :color="a1.color" :bg="a1.bg" :borderRadius="a1.borderRadius" :area="a1.area"></hard-corner>
   </div>
 </template>
 
 <script>
-import {hardCorner4} from '@/components/oasiscare'
+import {hardCorner} from '@/components/oasiscare'
 export default {
   props: {},
   data () {
@@ -72,7 +72,7 @@ export default {
   computed: {
   },
   components: {
-    hardCorner4
+    hardCorner
   },
   methods: {},
   created () {},
