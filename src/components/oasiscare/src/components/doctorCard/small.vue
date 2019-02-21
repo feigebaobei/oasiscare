5d248067599aa21a56d3117484c9ea62a8b8cb68
feigebaobei <18515195415@163.com>
2019-02-21 13:32:46 +0800

    init

---
+++ src/components/oasiscare/src/components/doctorCard/small.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="basic">
+    <div class="leaderBox">
+      <span>团长</span>
+    </div>
+    <div class="headerBox">
+      <img :src="compHeaderImg" alt="" class="headerImg">
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { icon } from '../../lib/picMap.js'
+// import hardCorner from '../button/hardCorner.vue'
+export default {
+  props: {
+    imgUrl: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    compHeaderImg () {
+      let img = this.imgUrl
+      if (img) {
+        return img
+      } else {
+        return icon.default.x2
+      }
+    }
+  },
+  components: {
+  },
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../assets/style/main.styl'
+
+.basic
+  display: flex
+  flex-wrap: wrap
+  background-color: $white
+  padding: $paddingTop $paddingRight $paddingBottom $paddingLeft
+
+</style>
