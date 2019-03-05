01bb6e7263ca761c4a374df383e55300aff979ca
feigebaobei <18515195415@163.com>
2019-03-05 19:41:38 +0800

    删除多余的代码

--- src/components/demo/titleArea.vue
+++ src/components/demo/titleArea.vue
@@ -16,142 +16,6 @@
     <title-area :title="extremity.title" :subTitle="extremity.subTitle" :moreText="extremity.moreText" :eventTypeMore="'s'" @s="ss"></title-area>
     <h2>属性说明</h2>
     <explain-table :tds="attrs"></explain-table>
-    <!-- <div class="tableBox"> -->
-<!--       <table class="attrExplainTable" width="100%" border="8" cellpadding="5" cellspacing="0" frame="box" rules="all">
-        <thead>
-          <tr>
-            <td>属性</td>
-            <th>类型</th>
-            <th class="detailValueTr">默认值</th>
-            <th>说明</th>
-            <th>版本要求</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>hasImg</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片是否有图片是否有图片是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>imgUrl</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>title</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>subTitle</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>moreText</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>hasMore</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>hasDirectionIcon</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>directionImgUrl</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>directionImgStatus</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>directionImgSwitchDirection</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>directionImgDirection</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>directionImgRotation</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>eventType</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>data</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>eventTypeMore</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>dataMore</td>
-            <td>Boolean</td>
-            <td>true</td>
-            <td>是否有图片</td>
-            <td>-</td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tfoot>
-      </table> -->
-    <!-- </div> -->
     <h2>相关issue</h2>
     <h2>发布日志</h2>
   </div>
@@ -198,9 +62,9 @@ export default {
         ['directionImgDirection', '[String, Number]', '0', '方向图片初始时的旋转角度值。不需要传入单位。单位是deg', '-'],
         ['directionImgRotation', '[String, Number]', '90', '方向图片切换方向时旋转的角度值。不需要传入单位。单位是deg', '-'],
         ['eventType', 'String', '', '点击该组件时触发的事件，如果需要给事件对应的方法传入参数需要配合<code>data</code>属性。', '-'],
-        ['data', 'Object', '{}', '点击该组件时触发的事件对应的方法的参数', '-'],
+        ['data', '-', '{}', '点击该组件时触发的事件对应的方法的参数', '-'],
         ['eventTypeMore', 'String', '', '点击该组件的更多按钮时触发的事件，如果需要给事件对应的方法传入参数需要配合<code>dataMore</code>属性。', '-'],
-        ['dataMore', 'Object', '{}', '点击该组件的更多按钮时触发的事件对应的方法的参数', '-'],
+        ['dataMore', '-', '{}', '点击该组件的更多按钮时触发的事件对应的方法的参数', '-']
       ]
     }
   },
@@ -229,10 +93,5 @@ export default {
 
     h2
       font-size: .36rem
-
-    .import
-      code
-        background-color: #d8d8d8
-        color: #4a1
 
 </style>
