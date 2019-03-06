1b2e2d450736674507c3b28818f36e755cb0006a
feigebaobei <18515195415@163.com>
2019-03-06 18:02:26 +0800

    写authentication的文档

--- src/components/demo/authentication.vue
+++ src/components/demo/authentication.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="authentication">
+    <div class="import">
+      <h2>引用</h2>
+      <code>import {authentication, authenMedicine} from '@/components/oasiscare'</code>
+    </div>
     <h2>authentication</h2>
     <h2>默认值</h2>
     <!-- <div class="inline"> -->
@@ -20,16 +24,25 @@
     </div>
     <h2>提示: 因为这个组件内部都是行内元素。所以这个组件必须做成块级元素。若不做为块级元素会受到父元素的字号样式影响。</h2>
     <h2>因此该组件只能做为块级元素使用</h2>
+    <h2>属性说明</h2>
+    <explain-table :tds="attrs0"></explain-table>
+    <h2>相关issue</h2>
+    <h2>发布日志</h2>
     <h2>authenMedicine</h2>
     <h2>默认值</h2>
     <authen-medicine></authen-medicine>
     <h2>自定义值</h2>
     <authen-medicine :text="a0.text"></authen-medicine>
+    <h2>属性说明</h2>
+    <explain-table :tds="attrs1"></explain-table>
+    <h2>相关issue</h2>
+    <h2>发布日志</h2>
   </div>
 </template>
 
 <script>
 import { authentication, authenMedicine } from '@/components/oasiscare'
+import explainTable from '../../components/explainTabel.vue'
 export default {
   props: {},
   data () {
@@ -42,14 +55,22 @@ export default {
       },
       a2: {
         text: 'welcome'
-      }
+      },
+      attrs0: [
+        ['imgUrl', 'String', '', '自定义输入的图片', '-'],
+        ['text', 'String', '全球医联体认证', '显示的文本', '-']
+      ],
+      attrs1: [
+        ['text', 'String', '认证', '显示的文本', '-']
+      ]
     }
   },
   computed: {
   },
   components: {
     authentication,
-    authenMedicine
+    authenMedicine,
+    explainTable
   },
   methods: {},
   created () {},
@@ -58,6 +79,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import '../../assets/style/basic.styl'
 
 .authentication
 
